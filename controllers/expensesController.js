03302f79c1a8cd01f46078f2c1ca6d946c407e30
VaibhavRajai <rajaivaibhav28@gmail.com>
2025-06-16 15:53:37 +0530

    get expenses

--- controllers/expensesController.js
+++ controllers/expensesController.js
@@ -21,7 +21,24 @@ const addExpense = async (req, res) => {
         return res.status(500).json({ message: error.message });
     }
 };
+const viewExpensesByUsername=async(req,res)=>{
+    await connect();
+    try{
+        const {username}=req.body;
+        if(!username){
+            return res.status(400).json({message:"Username is required"})
+        }
+        const expenses=await Expenses.fetch({username});
 
+        if(expenses.length==0){
+            return res.status(404).json({message:"No Exepenses yet!!"})
+        }
+        res.status(200).json({expenses})
+    }catch(error){
+        return res.status(500).json({message:error.message})
+    }
+}
 module.exports = {
-    addExpense
+    addExpense,
+    viewExpensesByUsername
 };
--- routes/expensesRoutes.js
+++ routes/expensesRoutes.js
@@ -4,4 +4,5 @@ const router=express.Router()
 const expensesController=require('../controllers/expensesController')
 
 router.post('/addExpense',expensesController.addExpense)
+router.post('/viewExpensesByUsername',expensesController.viewExpensesByUsername)
 module.exports=router;
